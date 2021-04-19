873e711ca32049016c6374054765eb6e2808f4d5
hbinr <1807227469@qq.com>
2021-04-19 19:50:29 +0800

    add:leetcode 88|300

--- code/00-LeetCode/array/easy_88_test.go
+++ code/00-LeetCode/array/easy_88_test.go
@@ -6,8 +6,8 @@ package array
 
 	https://leetcode-cn.com/problems/sorted-merge-lcci/submissions/
 */
-
-func merge(nums1 []int, m int, nums2 []int, n int) {
+//  方法一
+func merge1(nums1 []int, m int, nums2 []int, n int) {
 	// 存放合并后的结果
 	sorted := make([]int, 0, m+n)
 
@@ -36,4 +36,33 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	}
 	// 将合并后的结果拷贝nums1中
 	copy(nums1, sorted)
+}
+
+//  方法二 从后到前把nums1的位置根据比较大小的方式填满，然后如果nums2还有部分剩余没有被填进去，那就逐个填进去
+// https://leetcode-cn.com/problems/merge-sorted-array/solution/gu-du-ji-mo-jian-ji-golangshuang-100jie-0xpg0/
+func merge2(nums1 []int, m int, nums2 []int, n int) {
+	// i：nums1中 m个元素的最后一位
+	// j：nums2中 n个元素的最后一位
+	// cur：nums1最后一位
+	i, j, cur := m-1, n-1, m+n-1
+
+	for i >= 0 && j >= 0 {
+		// 如果nums遍历完，并且nums[i] > nums[j]，那么将较大的数放到nums1后面(即cur索引位置)
+		// 并且向前移动cur和i
+		if nums1[i] > nums2[j] {
+			nums1[cur] = nums1[i]
+			i--
+		} else { //
+			nums1[cur] = nums2[j]
+			j--
+		}
+		cur--
+	}
+
+	//当nums2未完全填充的时候
+	for j >= 0 {
+		nums1[cur] = nums2[j]
+		cur--
+		j--
+	}
 }
--- code/00-LeetCode/template/dp_test.go
+++ code/00-LeetCode/template/dp_test.go
@@ -1,3 +1,39 @@
 package template
 
-// 动态规划模板
+// 动态规划模板 : 一般适用于求最值
+
+/*
+	# 第一步：初识化base case
+	dp[0][0] = base  case
+	# 第二步：进行状态转移
+	for 状态1 in 状态1的所有值:
+		for 状态2 in 状态3的所有值:
+			for ...
+			# 第三步：套用表达式 (求最值)
+			dp[状态1][状态2][...] = 求最值(选择1, 选择2, ...)
+
+*/
+
+// 斐波那契数列
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	// dp table
+	dp := []int{}
+	// 第一步：base case, 注意：dp存储了所有的状态，特殊案例中可能值需要dp table中的一部分
+	dp[1], dp[2] = 1, 1
+
+	// 第二步：状态转移
+	for i := 3; i <= n; i++ {
+		// 第三步：套用表达式
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
+}

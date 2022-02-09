f17b5d82af094c55cec73458730ff08869cdb487
hbinr <dhb1807227469@.163.com>
2022-02-09 20:14:49 +0800

    code: binary search opt

--- code/06-Binary-Search-Tree/01-Binary-Search-Method/binary_search_test.go
+++ code/06-Binary-Search-Tree/01-Binary-Search-Method/binary_search_test.go
@@ -1,4 +1,16 @@
-package main
+package binnary_search_tree
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBinarySearch(t *testing.T) {
+
+	arr := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	res := binarySearch(arr, 4, 2)
+	fmt.Println("res:", res)
+}
 
 // 二分查找法，在有序数组arr中，查找target
 // 如果找到target，返回相应的索引index
@@ -15,23 +27,20 @@ func binarySearch(arr []int64, n, target int64) int64 {
 
 	for l <= r {
 
-		mid := (l + r) / 2
-		if arr[mid] == target {
-			return mid
-		}
+		mid := l + (r-l)>>1
+		// mid := (l + r) / 2
 
 		// 如果目标小于分界点
 		if target < arr[mid] {
 			// 则在arr[l,mid-1]之中查找
 			r = mid - 1
-		} else { // 如果目标大于分界点
+		} else if target > arr[mid] { // 如果目标大于分界点
 			// 则在arr[mid+1,r]之中查找
 			l = mid + 1
+		} else {
+			return mid
 		}
+
 	}
 	return -1
-}
-
-func main() {
-
 }
